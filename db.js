4dade305606301fe669b60178fec4846d88bc22d
Hadiyal Vijay <hadiyalvijay7777@gmail.com>
2025-03-10 12:37:17 +0530

    update commit

--- db.js
+++ db.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose');
 require('dotenv').config()
-const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/classroom';
+const dbUrl = process.env.DB_URL || 'mongodb+srv://admin:admin07@cluster0.mer5k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 const connectToMongo = () => {
     mongoose.set("strictQuery", false);
-    mongoose.connect(dbUrl)
-        .then(() => {
-            console.log("DATABASE CONNECTED")
-        })
-        .catch(err => {
-            console.log("OH NO ERROR!!!!")
-            console.log(err)
-        })
+    mongoose.connect(dbUrl, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log("DATABASE CONNECTED");
+    }).catch(err => {
+        console.error("OH NO ERROR!!!!", err.message);
+    });
 }
+
+
 module.exports = connectToMongo;
